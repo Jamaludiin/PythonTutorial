1673442587188bb12f3c8e231c440b5a18f61302
Jamal Abdullahi <jamalkacade@gmail.com>
2023-10-21 16:11:16 +0800

    A new folder called Float is created and it contains a file called float.py

---
+++ Float/float.py
@@ -0,0 +1,49 @@
+print("Example 0 ---------------------------------------------")
+# Create a float type variable in Python
+var_float1 = 6.89
+var_float2 = 4.89
+var_float3 = 89.6e-4
+
+print("\nExample 1 ---------------------------------------------")
+# Display the weight and height
+print(var_float1)
+print(var_float2)
+print(var_float3)
+
+print("\nExample 2 ---------------------------------------------")
+
+# Check their type
+print(type(var_float1))
+print(type(var_float2))
+print(type(var_float3))
+
+print("\nExample 3 ---------------------------------------------")
+
+# --------------------------
+# Create a float type variable in Python
+var_int = 6.89
+var_str = 4.89
+
+# use the float() function in Python
+var_float1 = float(var_int)
+var_float2 = float(var_str)
+var_float3 = float("30")
+var_float4 = float(55)
+
+# Display the weight and height
+print(var_float1)
+print(var_float2)
+print(var_float3)
+print(var_float4)
+print(float("4"))
+
+print("\nExample 4 ---------------------------------------------")
+
+# Check their type
+print(type(var_float1))
+print(type(var_float2))
+print(type(var_float3))
+print(type(var_float4))
+print(type(float("4")))
+
+# &lt;class 'float'&gt;
