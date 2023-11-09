4c8e2f60f756cd8bbbe1d6a0ce03e6e1d498b22b
Jamal Abdullahi <jamalkacade@gmail.com>
2023-11-09 15:01:04 +0800

    Lambda function practice file is added

---
+++ Lambda/lambda.py
@@ -0,0 +1,6 @@
+print(
+    "lambda arguments : expression---- A lambda function can take any number of arguments, but can only have one expression"
+)
+
+result = lambda x: x + 5
+print(result(10))
