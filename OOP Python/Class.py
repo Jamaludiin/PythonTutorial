fbcabb882d9e31e314a1ef4a03f7186596f4004d
jamal <jamalkacade@gmail.com>
2023-12-14 13:15:02 +0800

    function within a class that returns values

--- OOP Python/Class.py
+++ OOP Python/Class.py
@@ -25,3 +25,30 @@ class Person:
 p1 = Person("Alice", "Mike", 36)
 
 print(p1.fname, p1.lname, p1.age)
+
+
+# --------------------------------------------------------------------------------------
+# Class with return value
+class myReturnClass:
+   def __init__(self, name, age, phone):
+      self.name = name 
+      self.age = age
+      self.phone = phone
+      self.result = self.funcReturnInfor()
+
+   def funcReturnInfor(self):
+         if self.age > 18:
+            return "You are Mature Person"
+         elif self.age < 18:
+            print("Thank you")
+            return "You are Not Mature Person"
+
+print("\n")
+# Example usage
+result = myReturnClass("Nuh", 26, 683342782)
+print(f"{result.name} {result.age} {result.phone} = {result.result}")
+
+print("\n")
+# Example usage
+result = myReturnClass("Nuh", 17, 683342782)
+print(f"{result.name} {result.age} {result.phone} = {result.result}")
