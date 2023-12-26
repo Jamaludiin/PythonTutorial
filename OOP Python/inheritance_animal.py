05010c8f1018bf62ddb49d83ccb25be4c4ef1ef6
jamal <jamalkacade@gmail.com>
2023-12-26 18:43:01 +0800

    Inheritances are made in inheritance_aimal.py

---
+++ OOP Python/inheritance_animal.py
@@ -0,0 +1,55 @@
+# Inheriatnce class practice
+
+class animal:
+    def __init__(self, name, color, category):
+        self.name = name
+        self.color = color 
+        self.category = category 
+    
+
+    def show(self):
+        print(f"Name: {self.name}, \nColor: {self.color} \nCategory: {self.category} \nAnimal Facts:")
+        
+    def charecteristics(self):
+       print("This animal is from Unknown Cataegory\n")
+
+
+class mammals(animal): # inherited the animal class
+    def charecteristics(self):
+      print("Mammals are characterized by the presence of milk-producing mammary glands for feeding their young\n")
+
+class birds(animal):
+     def charecteristics(self):
+        print("Birds are characterised by feathers, toothless beaked jaws, the laying of hard-shelled eggs, a high metabolic rate, a four-chambered heart, and a strong yet lightweight skeleton.\n")
+       
+class fish(animal):
+     def charecteristics(self):
+        print("Fish or fishes is an aquatic, craniate, gill-bearing animal that lacks limbs with digits. \n")
+       
+class reptiles(animal):
+     def charecteristics(self):
+         print("Reptiles, as commonly defined, are a group of tetrapods with an ectothermic metabolism and amniotic development.\n")
+        
+
+            
+animal_obj = animal("Lion","Red", "Mammals")
+animal_obj.show()
+animal_obj.charecteristics()
+
+
+mammals_obj = mammals("Tiger","Blue", "Mammals")
+mammals_obj.show()
+mammals_obj.charecteristics()
+
+
+birds_obj = birds("Lion","Red", "birds")
+birds_obj.show()
+birds_obj.charecteristics()
+
+fish_obj = fish("Grouper","Yellow gold", "fish")
+fish_obj.show()
+fish_obj.charecteristics()
+
+reptiles_obj = reptiles("Snake","Green", "reptiles")
+reptiles_obj.show()
+reptiles_obj.charecteristics()
